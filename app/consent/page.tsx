0903115220b5db7e870714629709c8213e9a14e3
jororo9 <foyougod@naver.com>
2026-06-22 06:44:15 +0900

    서명 모달 스크롤 잠금

--- app/consent/page.tsx
+++ app/consent/page.tsx
@@ -139,6 +139,20 @@ export default function ConsentPage() {
   }
 
   useEffect(() => {
+    if (signModal) {
+      document.body.style.overflow = 'hidden'
+      document.body.style.touchAction = 'none'
+    } else {
+      document.body.style.overflow = ''
+      document.body.style.touchAction = ''
+    }
+    return () => {
+      document.body.style.overflow = ''
+      document.body.style.touchAction = ''
+    }
+  }, [signModal])
+
+  useEffect(() => {
     const updateSize = () => {
       if (noticeRef.current && highlightRef.current) {
         const rect = noticeRef.current.getBoundingClientRect()
